70696c8fce0c0c671af528ebb0638988ab4f36c7
Bryan Wyatt <brwyatt@gmail.com>
2016-01-30 11:51:34 -0800

    Remove ProxyFix - We will be proxy-aware

--- authservicesweb.py
+++ authservicesweb.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
   return "SUCCESS (from authservicesweb::root)\n"
-
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run()
