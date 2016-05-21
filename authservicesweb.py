c951e9f2e285ceace112abb7f8b1890156fb6b3e
Bryan Wyatt <brwyatt@gmail.com>
2016-05-21 11:47:53 -0700

    Clean in preparation of starting work

--- authservicesweb.py
+++ authservicesweb.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def root():
-  return "SUCCESS (from authservicesweb::root)\n"
+    return "SUCCESS (from authservicesweb::root)\n"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
